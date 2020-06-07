155ab90e54f7a7cd9bca0bd4b9b9a4bb46445144
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-07 17:38:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :not_clickbait
+
+  ARR = [
+    /Won't Believe/,
+    /Secret/,
+    /Top/,
+    /Guess/
+  ]
+
+    def not_clickbait
+      if ARR.none? { |rec| rec.match title}
+        errors.add(:title, "it must be clickbait")
+    end
+  end
 end
